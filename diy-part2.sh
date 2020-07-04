8aba66bcdd31cc07c812bc09158c26037c8c3feb
daier11 <67797478+daier11@users.noreply.github.com>
2020-07-05 00:47:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,5 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+sed -i 's/192.168.1.1/192.16.12.1/g' package/base-files/files/bin/config_generate
